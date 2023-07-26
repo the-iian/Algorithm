71b642cc290823ef29116d8447cca58f06527267
iian <dheyw@naver.com>
2023-07-26 13:59:25 +0900

    한번만 등장한 문자

--- PCCP/Characters only appear once.py
+++ PCCP/Characters only appear once.py
@@ -1,6 +1,10 @@
 def solution(s):
     answer = ''
     for i in s:
-        if s.count(i)==1:
+        if s.count(i) == 1:
             answer += i
-            return ''.join(sorted(answer))
+    return ''.join(sorted(answer))
+
+
+# python은 들여쓰기로 문법을 검증하므로 주의하자
+print(solution('hello'))
